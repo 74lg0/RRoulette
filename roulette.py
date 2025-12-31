fa0fdc1fa3d4b84b692f6ffbc3f5464d1aa0799b
74lg0 <hack-z4lg0@proton.me>
2025-12-30 22:04:12 -0600

    Todavia con BUG en la logica de la animacion

--- roulette.py
+++ roulette.py
@@ -4,11 +4,41 @@
 # Secrets es para mejor aleatorizacion ;)
 import secrets
 from pystyle import Colorate, Colors
+import time
+import os
 # from colorama import Fore # Aun no la voy a usar, por el momento
+
+def clear():
+    os.system('cls' if os.name == "nt" else 'clear')
+
+# Animación para la carga de balas
+def char_bull(b, bedrooms):
+    recamara = "┃"
+    bala = "█"
+
+    for b in range(b):
+        print(Colorate.Horizontal(Colors.rainbow, bala), end='')
+
+    for r in range(bedrooms - b):
+        print(Colorate.Horizontal(Colors.rainbow, recamara), end='')
+    
+    time.sleep(0.1)
+
 
 # Funcion para mostrar el estado actual de cada ronda
 def estado_actual(bot, user, bullets, bedrooms):
-    print(Colorate.Horizontal(Colors.rainbow, f"Vidas\nBOT : [{bot}]\nUSER : [{user}]"))
+    banner = f'''
+========== Vidas ==========
+BOT     : {bot}
+USER    : {user}
+===========================
+BALAS   : '''
+
+    # Animacion
+    for b in range(bedrooms):
+        clear()
+        print(Colorate.Horizontal(Colors.rainbow, banner), end='')
+        char_bull(bullets, b)
 
 # Funcion para calcular la probabilidad de morir
 def c_death(bullets, bedrooms):
@@ -38,10 +68,10 @@ def ai_bot(bullets, bedrooms):
 # Funcion para las rondas
 def rounds(r):
     # Variables iniciales
-    bot = 5
+    bot = 5 # Vidas iniciales = 5
     user = 5
-    bullets = 1
-    bedrooms = 6
+    bullets = 1 # Balas = (r) Por cada ronda se suma +1 bala
+    bedrooms = 6 #Cámaras, aquí son 6
 
     for ronda_actual in range(1, r + 1):
         estado_actual(bot, user, bullets, bedrooms)
